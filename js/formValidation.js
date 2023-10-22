be5eea4b562ffd918b134b6099f5fcab8c739bf4
Miras <bking4234@gmail.com>
2023-10-22 18:30:45 +0600

    add: add form validator for all pages

---
+++ js/formValidation.js
@@ -0,0 +1,21 @@
+const submitBtn = document.querySelector('#submit');
+const phoneInput = document.querySelector('#phone');
+const commentInput = document.querySelector('#comment')
+
+
+submitBtn.addEventListener('click', validatorForm);
+
+function validatorForm(event){
+    const regex = /[a-zA-Z]/;
+    const phoneValue = phoneInput.value;
+    const commentValue = commentInput.value;
+    
+    if(!phoneValue || !commentValue){
+        alert('Введите номер или комментарий');
+    }
+    if(regex.test(phoneValue)){
+        alert('Номер должен состоят из чисел')
+    }
+    
+    event.preventDefault();
+}
