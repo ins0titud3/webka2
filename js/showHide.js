4137e44ec89db20c6d13e4be13c342a97a17af29
Esimgali <esimgalikhamitov2005@gmail.com>
2023-10-23 01:44:25 +0600

    button color

--- js/showHide.js
+++ js/showHide.js
@@ -1,5 +1,14 @@
 function showFinished(){
     var finished = document.getElementById("projects").childNodes
+    var buttons = document.getElementById("sort").childNodes
+    for(let i = 0; i < buttons.length; i++){
+        if(buttons[i].id !== "finished"){
+            buttons[i].style = "background-color: white; color: #052c65;"
+        }
+        else{
+            buttons[i].style = "background-color: #ffac38;  color: white;"
+        }
+    }
     for(let i = 0; i < finished.length; i++){
         if(finished[i].id !== "finishedProj"){
             finished[i].style = "display: none"
@@ -19,6 +28,15 @@ function showInProcess(){
             finished[i].style = "display: "
         }
     }
+    var buttons = document.getElementById("sort").childNodes
+    for(let i = 0; i < buttons.length; i++){
+        if(buttons[i].id !== "inProcess"){
+            buttons[i].style = "background-color: white; color: #052c65;"
+        }
+        else{
+            buttons[i].style = "background-color: #ffac38;  color: white;"
+        }
+    }
 }
 function showDesignProcess(){
     var finished = document.getElementById("projects").childNodes
@@ -30,10 +48,28 @@ function showDesignProcess(){
             finished[i].style = "display: "
         }
     }
+    var buttons = document.getElementById("sort").childNodes
+    for(let i = 0; i < buttons.length; i++){
+        if(buttons[i].id !== "designProcess"){
+            buttons[i].style = "background-color: white; color: #052c65;"
+        }
+        else{
+            buttons[i].style = "background-color: #ffac38;  color: white;"
+        }
+    }
 }
 function showAll(){
     var finished = document.getElementById("projects").childNodes
     for(let i = 0; i < finished.length; i++){
             finished[i].style = "display: "
+    }
+    var buttons = document.getElementById("sort").childNodes
+    for(let i = 0; i < buttons.length; i++){
+        if(buttons[i].id !== "all"){
+            buttons[i].style = "background-color: white; color: #052c65;"
+        }
+        else{
+            buttons[i].style = "background-color: #ffac38;  color: white;"
+        }
     }
 }
