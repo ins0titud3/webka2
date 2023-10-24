799a08a3b074113674877f70ad978f8db9f7e30b
Esimgali <esimgalikhamitov2005@gmail.com>
2023-10-24 14:10:21 +0600

    open new window

--- js/messagesPprompts.js
+++ js/messagesPprompts.js
@@ -1,3 +1,14 @@
-function message(){
-    confirm("Вы уверены что хотите прейти на этот сайт?")
+function message  (event) {
+    var id = event.target.id
+    if(confirm("Вы уверены что хотите прейти на этот сайт?") ){
+        if(id == "instagram"){
+            window.open("https://www.instagram.com/", "_blank")
+        }
+        else if(id == "facebook"){
+            window.open("https://www.facebook.com/", "_blank")
+        }
+        else{
+            window.open("https://www.whatsapp.com/", "_blank")
+        }
+    }
 }
