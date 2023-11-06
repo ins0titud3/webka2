813d07be395113786ddc0951e2af0b8e83b10aec
Esimgali <esimgalikhamitov2005@gmail.com>
2023-11-07 05:05:26 +0600

    create json database валидация

--- js/jsons.js
+++ js/jsons.js
@@ -1,27 +1,77 @@
-const getTodoItems = async () => {
-    try{
-        // const response = await axios.get("https://console.firebase.google.com/project/webproj-429b0/database/webproj-429b0-default-rtdb/data/~2F")
-        // const todoItems = response.data
-        // console.log(todoItems);
-        // return todoItems
-        axios({
-          method: 'get',
-          url: 'https://console.firebase.google.com/project/webproj-429b0/database/webproj-429b0-default-rtdb/data/~2F',
-          responseType: 'stream'
-        })
-          .then(function (response) {
-            const inst = response.data
-            console.log(inst);
-          });
-    } catch (errors) {
-        console.error(errors);
-    }}
-    async function getUser() {
-        try {
-          const response = await axios.get("https://console.firebase.google.com/project/webproj-429b0/database/webproj-429b0-default-rtdb/data/~2F");
-          console.log(response);
-        } catch (error) {
-          console.error(error);
-        }
-      }
+async function createUser(){
+  e.preventDefault();
+  if(validation()){
     
+  }
+}
+async function login(){
+  const Login = document.querySelector().textContent
+  const Password = document.querySelector().textContent
+  const suc = false;
+  const response = await axios.get("https://webprojass4-default-rtdb.firebaseio.com/users.json")
+  response.data.map((obj, index) => {
+    if(obj.login == Login){
+      if(obj.password == Password){
+        suc = true;
+        return
+      }
+      else{
+        return
+      }
+    }
+  })
+  if(suc){
+    alert("Вы успешно прошли проверку!")
+  }
+  else{
+    alert("Вы неправильно ввели пароль или логин!")
+  }
+}
+
+function validation(){
+  e.preventDefault();
+  const isLogin = document.querySelector().title
+  const isPassword = document.querySelector().title
+  if(isLogin && isPassword){
+    return true
+  }
+  else{
+    return false
+  }
+}
+function validationOfUsername(){
+  e.preventDefault();
+  const loginInput = document.querySelector();
+  const res = document.querySelector();
+  loginInput.addEventListener("change", (event) => {
+    if(event.target.value >= 5){
+      res.textContent = "Логин прошла проверку"
+      res.title = true
+      return true;
+    }
+    else{
+      res.textContent ="Логин должно быть 5 символов"
+      res.title = false
+    }
+  });
+}
+function validationofPassword(){
+  e.preventDefault();
+  const passwordInput = document.querySelector();
+  const res = document.querySelector();
+  passwordInput.addEventListener("change", (event) => {
+    if(event.target.value >= 6){
+      res.textContent = "пароль прошла проверку"
+      res.title = true
+      return true;
+    }
+    else{
+      res.textContent ="Пароль должно быть 6 символов"
+      res.title = false
+    }
+  });
+}
+async function getUser() {
+  const res = await axios.get("https://webprojass4-default-rtdb.firebaseio.com/users.json")
+  res.data.map((obj, index) => {console.log(index, obj)});
+}
