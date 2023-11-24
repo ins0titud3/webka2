ba27187f4d29fd77d9886189c1dc83807fd34330
Esimgali <esimgalikhamitov2005@gmail.com>
2023-11-25 01:29:08 +0600

    add feature

--- js/login.js
+++ js/login.js
@@ -8,11 +8,11 @@ const uname = document.querySelector('#usernameLogin')
 const nameValid = document.createElement("p")
 uname.addEventListener("input", ()=>{
     if(uname.value.length < 6){
-        nameValid.innerHTML = "Username length should be more than 5"
+        notify(formGroup[0], "Username length should be more than 5")
     }
     else{
         isNameValid = true
-        nameValid.innerHTML = ""
+        notify(formGroup[0], "")
     }
 })
 
@@ -22,19 +22,32 @@ pass.addEventListener("input", ()=>{
     if(!(/[A-Z]/.test(pass.value) &&
     /[a-z]/ .test(pass.value) &&
     /[0-9]/.test(pass.value) &&
-    pass.value.length > 6)){
-        passValid.innerHTML = "'Password length should be more than 5 and contain uppercase and lowercase letters'"
+    pass.value.length > 5)){
+        notify(formGroup[1], "Password length should be more than 5 and contain uppercase and lowercase letters")
     }
     else{
         isPassValid = true
-        passValid.innerHTML = ""
+        notify(formGroup[1],"")
     }
 })
 
 
-
-formGroup[1].appendChild(passValid)
-formGroup[0].appendChild(nameValid)
+function notify(tag, text){
+    let add = true
+    for(const child of tag.children){
+        if(child.tagName == 'P'){
+            add = false
+        }
+    }
+    if(add){
+    const notif = document.createElement("p")
+    notif.innerHTML = text
+    tag.appendChild(notif)
+    }
+    else{
+        tag.children[2].innerHTML = text
+    }
+}
 
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -58,16 +71,15 @@ loginBtn.addEventListener('click', async (e)=>{
             console.log('nice');
             localStorage.setItem('key', key);
             location.reload();
-            incorrectLOgin.innerHTML = ""
+            notify(formGroup[1],"")
         }})
         if(!isCorrect){
-            incorrectLOgin.innerHTML = "Вы не правильно ввели пароль или имя пользователя"
+            notify(formGroup[1], "Вы не правильно ввели пароль или имя пользователя")
+            
         }
-        formGroup[1].appendChild(incorrectLOgin)
     }
     else{
         alert("Форма не валидна");
     }
 })
-
 
