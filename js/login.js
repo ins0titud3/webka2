845876bf91dbf1c8ab9746f0d3ae5768c7bab9d5
Esimgali <esimgalikhamitov2005@gmail.com>
2023-11-25 00:07:41 +0600

    fix validation

--- js/login.js
+++ js/login.js
@@ -7,9 +7,8 @@ const formGroup = document.querySelectorAll(".form-group")
 const uname = document.querySelector('#usernameLogin')
 const nameValid = document.createElement("p")
 uname.addEventListener("input", ()=>{
-    console.log(uname.value.length);
     if(uname.value.length < 6){
-        nameValid.innerHTML = "Невалидное имя"
+        nameValid.innerHTML = "Username length should be more than 5"
     }
     else{
         isNameValid = true
@@ -20,12 +19,11 @@ uname.addEventListener("input", ()=>{
 const pass = document.querySelector('#passwordLogin')
 const passValid =document.createElement("p")
 pass.addEventListener("input", ()=>{
-    console.log(pass.value.length);
     if(!(/[A-Z]/.test(pass.value) &&
     /[a-z]/ .test(pass.value) &&
     /[0-9]/.test(pass.value) &&
-    pass.value.length > 4)){
-        passValid.innerHTML = "Невалидный пароль"
+    pass.value.length > 6)){
+        passValid.innerHTML = "'Password length should be more than 5 and contain uppercase and lowercase letters'"
     }
     else{
         isPassValid = true
@@ -41,35 +39,34 @@ formGroup[0].appendChild(nameValid)
 
 window.addEventListener('DOMContentLoaded', async () => {
     user = await axios.get(`https://webprojass4-default-rtdb.firebaseio.com/users.json`)
-    console.log(user);
 })
 
 loginBtn.addEventListener('click', async (e)=>{
     e.preventDefault();
+
+    
+    const incorrectLOgin = document.createElement("p")
 
     const username = document.querySelector('#usernameLogin').value;
     const password = document.querySelector('#passwordLogin').value;
     let isCorrect = false
     if(isNameValid && isPassValid){
         const userData = user.data;
-    console.log(userData);
-    Object.keys(userData).map((key)=>{
+        Object.keys(userData).map((key)=>{
         if (userData[key].login ==  username && userData[key].password == password) {
             isCorrect = true
             console.log('nice');
             localStorage.setItem('key', key);
             location.reload();
+            incorrectLOgin.innerHTML = ""
+        }})
+        if(!isCorrect){
+            incorrectLOgin.innerHTML = "Вы не правильно ввели пароль или имя пользователя"
         }
-    }
-    )
+        formGroup[1].appendChild(incorrectLOgin)
     }
     else{
-        if(!isCorrect){
-            alert("Вы не правильно ввели пароль или имя пользователя");
-        }
-        else{
-            alert("Форма не валидна");
-        }
+        alert("Форма не валидна");
     }
 })
 
--- js/register.js
+++ js/register.js
@@ -5,23 +5,54 @@ async function createUser(e){
     const username = document.querySelector('#signupUsername')
     const email = document.querySelector('#email');
     const password = document.querySelector('#password')
+    const formGroup = document.querySelectorAll(".form-group")
+
+    let isNameValid = false
+    let isSurnameValid  = false
+    let isUsernameValid = false
+    let isEmailValid = false
+    let isPassValid = false
+
     if(!name.value && !surname.value && !username.value && !password.value && !email.value){
         alert('Fill the form')
     }else{
-        if(password.value.length <= 5){
-            alert('Password length should be more than 5')
-        }
-        else if(name.value.length <= 3){
-            alert('Name length should be more than 5')
-        }
-        else if(surname.value.length <= 3){
-            alert('Surname length should be more than 5')
-        }
-        else if(username.value.length <= 3){
-            alert('Username length should be more than 5')
-        }
-        else{
-            await axios.post(
+    if(!email.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
+        notify(formGroup[5], "Invalid email address")
+    }
+    else{
+        isEmailValid = true
+        notify(formGroup[5], "")}
+
+    if(!(/[A-Z]/.test(password.value) && /[a-z]/.test(password.value) && /[0-9]/.test(password.value) && password.value.length > 5)){
+        notify(formGroup[6], 'Password length should be more than 5, contain numbers, uppercase and lowercase letters')
+    }else{
+        isPassValid = true
+        notify(formGroup[6], "")
+    }
+
+    if(name.value.length < 6){
+        notify(formGroup[2], 'Name length should be more than 5')
+    }else{
+        isNameValid = true
+        notify(formGroup[2], "")
+    }
+    
+    if(surname.value.length < 6){
+        notify(formGroup[3], 'Surname length should be more than 5')
+    }else{
+        isSurnameValid = true
+        notify(formGroup[3], "")
+    }
+    
+    if(username.value.length < 6){
+        notify(formGroup[4], 'Username length should be more than 5')
+    }
+    else{
+        isUsernameValid = true
+        notify(formGroup[4], "")
+    }
+    if(isEmailValid && isNameValid && isPassValid && isSurnameValid && isUsernameValid){
+        await axios.post(
                 "https://webprojass4-default-rtdb.firebaseio.com/users.json",
                 {
                     "userName":name.value,
@@ -31,8 +62,24 @@ async function createUser(e){
                     "email":email.value,
                 }
             )
-            location.reload();
+            location.reload();       
+    }}
+}
+
+function notify(tag, text){
+    let add = true
+    for(const child of tag.children){
+        if(child.tagName == 'P'){
+            add = false
         }
+    }
+    if(add){
+    const notif = document.createElement("p")
+    notif.innerHTML = text
+    tag.appendChild(notif)
+    }
+    else{
+        tag.children[2].innerHTML = text
     }
 }
 
